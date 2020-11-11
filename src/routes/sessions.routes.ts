96727b80167f648366ca0bf34a7b2df2dc996e67
Michel Popolin de Freitas <micpopolin@gmail.com>
2020-11-11 17:03:53 -0300

    bak

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -2,7 +2,21 @@ import { Router } from 'express';
 
 import AuthenticateUserService from '../services/AuthenticateUserService';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { Aki } = require('aki-api');
+
+const region = 'pt';
+
+const aki = new Aki(region);
+
 const sessionsRouter = Router();
+
+sessionsRouter.get('/', async (request, response) => {
+    await aki.start();
+
+    const { question, answers } = aki;
+    return response.json({ question, answers });
+});
 
 sessionsRouter.post('/', async (request, response) => {
     const { email, password } = request.body;
