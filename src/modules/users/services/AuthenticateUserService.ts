e6cf20204387ed8c67c238c3191fc322b0e26ca9
Michel Popolin de Freitas <micpopolin@gmail.com>
2020-11-25 22:52:40 -0300

    aa

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,4 +1,3 @@
-import { compare } from 'bcryptjs';
 import { injectable, inject } from 'tsyringe';
 import { sign } from 'jsonwebtoken';
 
@@ -30,29 +29,24 @@ class AuthenticateUserService {
     ) {}
 
     public async execute({ email, password }: IRequest): Promise<IResponse> {
-        console.log({ email, password });
         const user = await this.usersRepository.findByEmail(email);
         if (!user) {
             throw new AppError('Incorrent email/password combination.', 401);
         }
-        console.log(2);
 
         const passwordMatched = await this.hashProvider.compareHash(
             password,
             user.password,
         );
-        console.log(3);
         if (!passwordMatched) {
             throw new AppError('Incorrent email/password combination.', 401);
         }
-        console.log(4);
         const { secret, expiresIn } = authConfig.jwt;
         const token = sign({}, secret, {
             subject: user.id,
             expiresIn,
         });
 
-        console.log(5);
         return { user, token };
     }
 }
